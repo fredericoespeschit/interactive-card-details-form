85d7612925bd3da1bb0d71a8542853f6bf8352c0
fredericoespeschit <fespeschitt@gmail.com>
2023-05-02 22:09:46 -0300

    Primeiros passos com JS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Bora pra cima!')
+const inputCardholderName = document.querySelector('#cardholderName');
+const inputCardNumber = document.querySelector('#cardNumber');
+const inputMonth = document.querySelector('#month');
+const inputYear = document.querySelector('#year');
+const buttonConfirm = document.querySelector('#button-confirm');
+
+buttonConfirm.addEventListener('click', function(event){
+  event.preventDefault();
+  console.log('clicou no botão em')
+})
