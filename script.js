55a0371d2e20e052c6020b64c2bd29e7b26b6ec7
fredericoespeschit <fespeschitt@gmail.com>
2023-05-03 21:48:06 -0300

    Recebendo dados com JS e exibindo em uma TAG Result

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const inputCardholderName = document.querySelector('#cardholderName');
-const inputCardNumber = document.querySelector('#cardNumber');
-const inputMonth = document.querySelector('#month');
-const inputYear = document.querySelector('#year');
 const buttonConfirm = document.querySelector('#button-confirm');
 
-buttonConfirm.addEventListener('click', function(event){
+const result = document.querySelector('#result');
+
+buttonConfirm.addEventListener('click', function (event) {
   event.preventDefault();
+
+  const inputCardholderName = document.querySelector('#cardholderName').value;
+  const inputCardNumber = document.querySelector('#cardNumber').value;
+  const inputMonth = document.querySelector('#month').value;
+  const inputYear = document.querySelector('#year').value;
+  const inputCVC = document.querySelector('#cvc').value;
+
+
+  result.textContent= inputCardholderName + ' - ' + inputCardNumber + ' - ' + inputMonth + ' - ' + inputYear + ' - ' + inputCVC;
+
+
+
+  console.log(result)
   console.log('clicou no botão em')
 })
