a3dc225a45fb24401a8d1d565dd90ae2215aecbd
fredericoespeschit <fespeschitt@gmail.com>
2023-05-04 20:13:57 -0300

    Primeiros passos com Eventos

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const buttonConfirm = document.querySelector('#button-confirm');
-
+const inputCardholderName = document.querySelector('#cardholderName').value;
+const inputCardNumber = document.querySelector('#cardNumber').value;
+const inputMonth = document.querySelector('#month').value;
+const inputYear = document.querySelector('#year').value;
+const inputCVC = document.querySelector('#cvc').value;
 const result = document.querySelector('#result');
 
 buttonConfirm.addEventListener('click', function (event) {
   event.preventDefault();
 
-  const inputCardholderName = document.querySelector('#cardholderName').value;
-  const inputCardNumber = document.querySelector('#cardNumber').value;
-  const inputMonth = document.querySelector('#month').value;
-  const inputYear = document.querySelector('#year').value;
-  const inputCVC = document.querySelector('#cvc').value;
-
-
-  result.textContent= inputCardholderName + ' - ' + inputCardNumber + ' - ' + inputMonth + ' - ' + inputYear + ' - ' + inputCVC;
-
 
 
   console.log(result)
   console.log('clicou no botão em')
+})
+
+document.addEventListener('keyup', function(e) {
+
+
+
+  // result.textContent= inputCardholderName + ' - ' + inputCardNumber + ' - ' + inputMonth + ' - ' + inputYear + ' - ' + inputCVC;
+
+  console.log(e.key)
 })
