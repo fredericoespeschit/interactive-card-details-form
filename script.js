fe943afb3f2f8c6793765a48bce363fb52ac4e96
fredericoespeschit <fespeschitt@gmail.com>
2023-05-06 01:41:48 -0300

    Criação dos IDs que iram receber o resultado dos valores inseridos nos inputs e funções para exibição no html

--- script.js
+++ script.js
@@ -6,29 +6,51 @@ const inputYear = document.querySelector('#year');
 const inputCVC = document.querySelector('#cvc');
 const result = document.querySelector('#result');
 
+//Output
+const OutputinputCardholderName = document.querySelector('#OutputinputCardholderName');
+const OutputinputCardNumber = document.querySelector('#OutputinputCardNumber');
+const OutputinputMonth = document.querySelector('#OutputinputMonth');
+const OutputinputYear = document.querySelector('#OutputinputYear');
+const OutputinputCVC = document.querySelector('#OutputinputCVC');
 
 
 inputCardholderName.addEventListener("keyup", (e) => {
   if (!e.target.value) {
-    result.innerHTML = "e.g. Jane Appleseed";
+    OutputinputCardholderName.innerHTML = "e.g. Jane Appleseed";
   } else {
-    result.innerHTML = e.target.value.toUpperCase();
+    OutputinputCardholderName.innerHTML = e.target.value.toUpperCase();
   }
 })
 
 inputCardNumber.addEventListener("keyup", (event) => {
   if (!event.target.value) {
-    result.innerHTML = "e.g 1234 5678 9123 0000";
+    OutputinputCardNumber.innerHTML = "e.g 1234 5678 9123 0000";
   } else {
-    result.innerHTML = event.target.value;
+    OutputinputCardNumber.innerHTML = event.target.value;
   }
 })
 
-inputMonth.addEventListener("Keyup", (e) => {
-  if (!e.target.value){
-    result.innerHTML = "MM";
+inputMonth.addEventListener("keyup", (ev) => {
+  if (!ev.target.value) {
+    OutputinputMonth.innerHTML = "YY";
   } else {
-    result.innerHTML = e.target.value;
+    OutputinputMonth.innerHTML = ev.target.value;
+  }
+})
+
+inputYear.addEventListener("keyup", (evento) => {
+  if (!evento.target.value) {
+    OutputinputYear.innerHTML = "YY";
+  } else {
+    OutputinputYear.innerHTML = evento.target.value;
+  }
+})
+
+inputCVC.addEventListener("keyup", (evn)=> {
+  if(!evn.target.value){
+    OutputinputCVC.innerHTML = "e.g. 123"
+  } else {
+    OutputinputCVC.innerHTML = evn.target.value;
   }
 })
 
